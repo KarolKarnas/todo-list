63ce01c1ca6ba2de56467dcc12d4dda2c77b2d4a
KarolKarnas <karol.karnas@gmail.com>
2023-02-08 12:49:09 +0100

    Create show/hide todo modal
    add event listeners to add / remove class .active on modal container to buttons and overlay

--- src/side-bar.js
+++ src/side-bar.js
@@ -1,14 +1,54 @@
 import { projects, ulAllProjects } from './projects';
 
-function createProjectsList() {
+// create navigation projects
 
-projects.forEach((proj) => {
-    const projectItemMenu = document.createElement('li');
-    projectItemMenu.textContent = proj.name;
-    // console.log(projectItemMenu);
-    ulAllProjects.appendChild(projectItemMenu);
+function createProjectsList() {
+	projects.forEach((proj) => {
+		const projectItemMenu = document.createElement('li');
+		projectItemMenu.textContent = proj.name;
+		// console.log(projectItemMenu);
+		ulAllProjects.appendChild(projectItemMenu);
+	});
+}
+
+// MODAL
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+	});
+});
+
+overlay.addEventListener('click', () => {
+const modals = document.querySelectorAll('.modal.active');
+modals.forEach (modal => {
+    closeModal(modal)
+})
 })
 
+closeModalButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		const modal = button.closest('.modal');
+		closeModal(modal);
+	});
+});
+
+function openModal(modal) {
+	if (modal == null) return;
+	modal.classList.add('active');
+	overlay.classList.add('active');
 }
+
+function closeModal(modal) {
+	if (modal == null) return;
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+}
+
 
 export default createProjectsList;
